1f6ab301369c868d63270c7ce75a47f60208bd7d
Mikey Sarty <sartintarm@gmail.com>
2025-05-26 00:47:40 -0700

    adding nuxt

---
+++ nuxt/nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  buildModules; [
+    'nuxt-vite',
+  ]
+modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxt/icon'
+  ]
+})
